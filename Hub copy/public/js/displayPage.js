48bce068dcfc8c44df1cb55dda8cf3467430ccd7
HPunchi <63794068+HPunchi@users.noreply.github.com>
2024-03-17 18:38:06 +0000

    Delete Hub copy directory

--- Hub copy/public/js/displayPage.js
+++
@@ -1,8 +0,0 @@
-function returnButtonClick(){
-    const tokenValue = sessionStorage.getItem("hsli_token"); 
-    if (tokenValue === null){
-        location.href = "/"; //login in again if no token is found
-    }else{
-        location.href = "/control-centre"; //redirect to control centre if token is there
-    }
-}
